71e45e01e77dab986ddb930c189a26a9a50b1f36
fengyuanliang3 <fengyuanliang2@jd.com>
2021-03-03 15:31:53 +0800

    chore: package

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 'use strict'
 
-const register = require('babel-register')
 const state = require('./lib/state')
 
-register({
-  plugins: ['transform-decorators-legacy']
-})
-
-module.exports = app => {
+module.exports = (app) => {
   state.app = app
 }
